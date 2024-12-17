c7ef31839d34a1b325ee024677215dc70b99bc23
Md Imon Hossain <imoncoc0@gmail.com>
2024-12-17 14:42:17 +0600

    hero section added

--- src/app/shared/components/home/hero-section/hero-section.component.ts
+++ src/app/shared/components/home/hero-section/hero-section.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-hero-section',
   templateUrl: './hero-section.component.html',
-  styleUrls: ['./hero-section.component.scss']
+  styleUrls: ['./hero-section.component.scss'],
 })
 export class HeroSectionComponent implements OnInit {
-
-  constructor() { }
+  constructor(private title: Title) {}
 
   ngOnInit(): void {
+    this.title.setTitle('Quick Mart | Home');
   }
-
 }
