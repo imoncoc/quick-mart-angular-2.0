75c6472faeb001aaa2e8eaa9f2b3c032dc638df4
Md Imon Hossain <imoncoc0@gmail.com>
2024-12-17 14:59:41 +0600

    signup added

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+  toggleConfirmPasswordVisibility() {
+    this.showConfirmPassword = !this.showConfirmPassword;
   }
 
+  constructor(private title: Title) {}
+
+  ngOnInit(): void {
+    this.title.setTitle('Quick Mart | Signup');
+  }
 }
