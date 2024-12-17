ec88c5b28a40f428328403c4e5590ccaf882d227
Md Imon Hossain <imoncoc0@gmail.com>
2024-12-17 14:32:42 +0600

    navbar and folder structure created

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroSectionComponent } from './components/home/hero-section/hero-section.component';
+import { AppRoutingModule } from '../app-routing.module';
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    HeroSectionComponent,
+  ],
+  imports: [CommonModule, AppRoutingModule],
+  exports: [NavbarComponent, FooterComponent],
+})
+export class SharedModule {}
