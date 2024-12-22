3967e468aedd7652f3fad6d9630202c9887a32fd
Md Imon Hossain <imoncoc0@gmail.com>
2024-12-23 00:21:08 +0600

    our-plan and service section added

---
+++ src/app/shared/components/home/our-partner/our-partner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-our-partner',
+  templateUrl: './our-partner.component.html',
+  styleUrls: ['./our-partner.component.scss']
+})
+export class OurPartnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,8 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeroSectionComponent } from './components/home/hero-section/hero-section.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { OurPartnerComponent } from './components/home/our-partner/our-partner.component';
+import { OurPlanComponent } from './components/home/our-plan/our-plan.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import { AppRoutingModule } from '../app-routing.module';
     FooterComponent,
     HomeComponent,
     HeroSectionComponent,
+    OurPartnerComponent,
+    OurPlanComponent,
   ],
   imports: [CommonModule, AppRoutingModule],
   exports: [NavbarComponent, FooterComponent],
